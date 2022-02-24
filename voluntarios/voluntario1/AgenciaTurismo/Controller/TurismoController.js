6191781820fcf4021f16a57cef95af480da2735d
Heberty Guelfi do Prado <hebertyprado22005@gmail.com>
2022-02-24 06:15:17 -0300

    att

--- voluntarios/voluntario1/AgenciaTurismo/Controller/TurismoController.js
+++ voluntarios/voluntario1/AgenciaTurismo/Controller/TurismoController.js
@@ -1,15 +1,16 @@
 class TurismoController {
+  constructor() {
+    this._country = document.getElementById("country");
+    this._city = document.getElementById("city");
+    this._people = document.getElementById("people");
+    this._salary = document.getElementById("salary");
+    this._airport = document.getElementById("airport");
+  }
 
-    constructor(){
-        this._city = document.getElementById("city");
-        this._country = document.getElementById("country");
-        this._people = document.getElementById("people");
-        this._salary = document.getElementById("salary");
-        this._airport = document.getElementById("airport");
-    }
+  exibe(event) {
+    event.preventDefault();
+    let varr = new Turismo;
 
-    exibe(event) {
-        event.preventDefault();
-        console.log("Nome de cidade que vc incluiu: " +  this._city.value);
-    }
+    console.log(varr);
+  }
 }
---
+++ voluntarios/voluntario1/AgenciaTurismo/Models/Turismo.js
@@ -0,0 +1,13 @@
+class Turismo {
+    constructor(country, city, people, salary, airport){
+        this._country.value = country;
+        this._city.value = city;
+        this._people.value = people;
+        this._salary.value = salary;
+        this._airport.value = airport;
+    }
+
+    aparece(){
+        console.log("Funcionou !");
+    }
+}
